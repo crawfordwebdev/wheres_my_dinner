91339be2c82bd8aebf8d9ae609ff1440155acb18
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-08-08 20:47:17 -0600

    adds routes for views animals animal detail animal create animal update animal delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
   path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('accounts/signup/', views.signup, name='signup'),
+  path('animals/', views.animals_index, name='animals_index'),
+  path('animals/<int:animal_id>/', views.animals_detail, name='animals_detail'),
+  path('animals/create/', views.AnimalCreate.as_view(), name='animals_create'),
+  path('animals/<int:pk>/update/', views.AnimalUpdate.as_view(), name='animals_update'),
+  path('animals/<int:pk>/delete/', views.AnimalDelete.as_view(), name='animals_delete'),
 ]
