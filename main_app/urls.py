1d224b6b3396dbde97e9bc31960fc7ed6c20a8f2
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-08-09 20:30:01 -0600

    adds medication update, delete and feeding create, update, and delete routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -12,4 +12,9 @@ urlpatterns = [
   path('animals/<int:pk>/delete/', views.AnimalDelete.as_view(), name='animals_delete'),
   path('animals/<int:animal_id>/add_photo/', views.add_photo, name='add_photo'),
   path('animals/<int:animal_id>/add_medication/', views.add_medication, name='add_medication'),
+  path('medication/<int:pk>/update/', views.MedicationUpdate.as_view(), name='medications_update'),
+  path('medication/<int:pk>/delete/', views.MedicationDelete.as_view(), name='medications_delete'),
+  path('feeding/create/', views.FeedingCreate.as_view(), name='feedings_create'),
+  path('feeding/<int:pk>/update/', views.FeedingUpdate.as_view(), name='feedings_update'),
+  path('feeding/<int:pk>/delete/', views.FeedingDelete.as_view(), name='feedings_delete'),
 ]
