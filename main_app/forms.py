36ff19bb4321853b4e0a1fdb0aa0d44065252790
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-08-09 21:32:00 -0600

    adds feeding, care log and weight forms

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,22 @@
 from django.forms import ModelForm
-from .models import Medication
+from .models import Medication, Feeding, Care_Log, Weight
 
 class MedicationForm(ModelForm):
   class Meta:
     model = Medication
     fields = ['name', 'dosage', 'frequency', 'description', 'end_date']
+
+class FeedingForm(ModelForm):
+  class Meta:
+    model = Feeding
+    fields = ['date', 'description']
+
+class Care_LogForm(ModelForm):
+  class Meta:
+    model = Care_Log
+    fields = ['date', 'description']
+
+class WeightForm(ModelForm):
+  class Meta:
+    model = Weight
+    fields = ['date', 'description']
